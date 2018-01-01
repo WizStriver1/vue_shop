ab4443faaa6c537e36430349a2ade32d94d9d45d
WizStriver1 <1954620922@qq.com>
2018-01-01 12:18:42 +0800

    连接mongodb商品列表查询接口

--- server/app.js
+++ server/app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var ejs = require("ejs");
+var ejs = require('ejs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var goods = require('./routes/goods');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/goods', goods);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
